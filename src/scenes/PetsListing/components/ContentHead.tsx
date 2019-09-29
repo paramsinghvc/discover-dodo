bff7ceaa8f1925fb824adff2a487ff38dc3a3501
Parminder Singh <parminder.singh1@swiggy.in>
2019-09-29 21:59:29 +0530

    Styling

--- src/scenes/PetsListing/components/ContentHead.tsx
+++ src/scenes/PetsListing/components/ContentHead.tsx
@@ -8,7 +8,7 @@ import IconButton from "@material-ui/core/IconButton";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    background: "white",
+    background: "rgba(255, 255, 255, 0.7)",
     border: 0,
     borderRadius: 3,
     boxShadow: "0 3px 5px 0px rgb(220,220,220)",
